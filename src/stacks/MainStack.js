7b9a323d1dafa2abc6b7080001fe01c20ccf6814
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2020-12-26 22:45:45 -0300

    first commit

---
+++ src/stacks/MainStack.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Preload from '../screens/Preload'
+import SingIn from '../screens/SingIn'
+import SingUp from '../screens/SingUp'
+
+const Stack = createStackNavigator();
+
+export default () =>(
+    <Stack.Navigator>
+        <Stack.Screen name="Preload" component={Preload} />
+        <Stack.Screen name="SingIn" component={SingIn} />
+        <Stack.Screen name="SingUp" component={SingUp} />
+    </Stack.Navigator>
+)
